f0e8f74a4236bf3e7e04c67672271ce277a1bf38
Laurenducay <laureneducay@gmail.com>
2025-01-09 12:35:28 -0800

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,9 @@
+let age = 20
+const myName = "Lauren"
+var animal = "cats"
+
+console.log(age)
+console.log(myName)
+console.log(animal)
+age++;
+console.log("Hello! My name is " + myName + " and I am " + age + " years old. I love " + animal + "!!")
