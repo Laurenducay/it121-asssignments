28e46487063a3e9b6b4e89c86d4d82f689428083
Laurenducay <laureneducay@gmail.com>
2025-01-14 11:12:45 -0800

    Add files via upload

---
+++ assignment5.js
@@ -0,0 +1,14 @@
+let minus = 5 - 3;
+console.log(minus);
+
+let add = 100 + "1";
+console.log(add);
+
+let mult = 50 * 3;
+console.log(mult);
+
+let div = 3/2;
+console.log(div);
+
+let mod = 10/5;
+console.log(mod);
