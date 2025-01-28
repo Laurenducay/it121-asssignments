75fd903d3b34590d441f207ab900654459283b3d
Laurenducay <laureneducay@gmail.com>
2025-01-28 10:24:08 -0800

    Add files via upload

---
+++ assignment7.js
@@ -0,0 +1,29 @@
+let x = 5;
+console.log('True Statements');
+
+console.log(x == 5);
+console.log(x === 5);
+console.log(x != 11);
+console.log(x !== 2);
+console.log(x > 4);
+console.log(x < 6);
+console.log(x >= 5);
+console.log(x <= 10);
+console.log(x == 5 && x > 3);
+console.log(x == 2 || x == 5);
+console.log(!(x == 1));
+
+console.log('------------------------');
+console.log('False Statements');
+
+console.log(x == 2);
+console.log(x === 4);
+console.log(x != 5);
+console.log(x !== 5);
+console.log(x > 10);
+console.log(x < 2);
+console.log(x >= 6);
+console.log(x <= -1);
+console.log(x == 3 && x > 7);
+console.log(x == 3 || x == 8);
+console.log(!(x == 5));
